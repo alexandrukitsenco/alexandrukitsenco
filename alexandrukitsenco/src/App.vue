e17b9cf9a82dd31c0a071389d1ac40edc5c07d97
achitenco <alexandru.kitsenco@gmail.com>
2022-07-29 14:14:41 +0200

    Initial Project
    Initial project made with vue2 and added firebase and vuetify

---
+++ alexandrukitsenco/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Main/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Main from './views/Main.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Main
+},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ alexandrukitsenco/src/views/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header/>
+  </div>
+</template>
+
+<script>
+import Header from '../components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Header
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
